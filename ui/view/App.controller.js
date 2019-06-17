f537eb1a63a668c8af9a07b726d22c75991a4ad0
Yao <shichun.yao@capgemini.com>
2019-06-17 21:32:27 +0800

    add datefield fragment template

--- ui/view/App.controller.js
+++ ui/view/App.controller.js
@@ -4,6 +4,19 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("com.sap.sme.x4.uidesigner.view.App", {
+		onInit: function() {
+			var data = {
+				label:"dateName",
+				dateValue: new Date()
+			};
+			var jsonModel = new sap.ui.model.json.JSONModel(data);
+			this.getView().setModel(jsonModel, "section");
+			var xmlFragment = sap.ui.xmlfragment("com.sap.sme.x4.uidesigner.view.fragment.DataField",
+									this);
+			var section = this.getView().byId("ObjectPageLayout").getSections()[0]
+			section.getAggregation("subSections")[0].addBlock(xmlFragment[0])
+			section.getAggregation("subSections")[0].addBlock(xmlFragment[1])
+		}
 
 	});
 });
